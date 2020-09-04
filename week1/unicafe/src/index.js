a33e0fc151bad7e18b94c3c362f5d542ec19c53d
Banskumansku <jan.lindholm@helsinki.fi>
2020-09-04 12:47:56 +0300

    1.6-1.10

---
+++ week1/unicafe/src/index.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const StatisticsLine = ({text, value}) => {
+  return (<div>{text} {value}</div>)
+}
+
+// oikea paikka komponentin määrittelyyn
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const ave = (good - bad) / all;
+  const pos = (good / all) * 100;
+  const positive = pos + ' %'
+  return (
+    <div>
+      <h1>Stats</h1>
+      <StatisticsLine text="good" value={good}/>
+      <StatisticsLine text="neutral" value={neutral}/>
+      <StatisticsLine text="bad" value={bad}/>
+      <StatisticsLine text="all" value={all}/>
+      <StatisticsLine text="average" value={ave}/>
+      <StatisticsLine text="positive" value={positive}/>
+    </div >
+  )
+}
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setToGood = newValue => {
+    setGood(newValue)
+  }
+  const setToNeutral = newValue => {
+    setNeutral(newValue)
+  }
+  const setToBad = newValue => {
+    setBad(newValue)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setToGood(good + 1)} text="good" />
+      <Button handleClick={() => setToNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setToBad(bad + 1)} text="bad" />
+      <div>
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
